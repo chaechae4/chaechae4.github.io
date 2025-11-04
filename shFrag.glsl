4acddb5ffbacb98295fa00ae01efdf44ba96b927
chaechae4 <109894824+chaechae4@users.noreply.github.com>
2025-11-04 17:13:08 +0900

    Add files via upload

--- shFrag.glsl
+++ shFrag.glsl
@@ -1,9 +1,48 @@
 #version 300 es
 
-precision mediump float;
-in vec4 v_color;
-out vec4 fragColor;
+precision highp float;
+
+struct Material {
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 vFragPos;
+in vec3 vNormal;
+in vec3 lightingColor;
+out vec4 FragColor;
+
+uniform vec3 u_viewPos;
+
+uniform Material material;
+uniform Light light;
+uniform int u_renderingMode;
 
 void main() {
-    fragColor = v_color;
-} 
+    if (u_renderingMode == 0) {
+        FragColor = vec4(lightingColor, 1.0);
+    }
+    else {
+        vec3 N = normalize(vNormal);
+        vec3 L = normalize(light.position - vFragPos);
+        vec3 V = normalize(u_viewPos - vFragPos);
+        vec3 R = reflect(-L, N);
+
+        float ndotl = max(dot(N, L), 0.0);
+
+        vec3 ambient  = light.ambient  * material.diffuse;
+        vec3 diffuse  = light.diffuse  * material.diffuse * ndotl;
+        float spec    = (ndotl > 0.0) ? pow(max(dot(V, R), 0.0), material.shininess) : 0.0;
+        vec3 specular = light.specular * material.specular * spec;
+
+        FragColor = vec4(ambient + diffuse + specular, 1.0);
+    }
+}
--- shVert.glsl
+++ shVert.glsl
@@ -1,12 +1,63 @@
 #version 300 es
-precision mediump float;
 
-in vec2 a_position;
-in vec4 a_color;
-uniform mat4 u_transform;
-out vec4 v_color;
+layout(location = 0) in vec3 a_position;
+layout(location = 1) in vec3 a_normal;
+layout(location = 2) in vec4 a_color;
+layout(location = 3) in vec2 a_texCoord;
+
+
+out vec3 vFragPos;
+out vec3 vNormal;
+out vec3 lightingColor; // resulting color at each vertex, to fragment shader
+
+uniform mat4 u_model;
+uniform mat4 u_view;
+uniform mat4 u_projection;
+
+struct Material {
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Material material;
+uniform Light light;
+uniform vec3 u_viewPos;
+
+uniform int u_renderingMode; 
 
 void main() {
-    gl_Position = u_transform * vec4(a_position, 0.0, 1.0);
-    v_color = a_color;
+    vec4 worldPos = u_model * vec4(a_position, 1.0);
+    vFragPos = worldPos.xyz;
+
+    mat3 normalMat = mat3(transpose(inverse(u_model)));
+    vec3 N = normalize(normalMat * a_normal);
+    vNormal = N;
+
+    // ambient
+    vec3 rgb = material.diffuse;
+    vec3 ambient = light.ambient * rgb;
+
+    // diffuse
+    vec3 L = normalize(light.position - vFragPos);
+    float ndotl = max(dot(N, L), 0.0);
+    vec3 diffuse = light.diffuse * ndotl * rgb;
+
+    // specular
+    vec3 V = normalize(u_viewPos - vFragPos);
+    vec3 R = reflect(-L, N);
+    float spec = (ndotl > 0.0) ? pow(max(dot(V, R), 0.0), material.shininess) : 0.0;
+    vec3 specular = light.specular * material.specular * spec;  
+
+    // ambient + diffuse + specular
+    lightingColor = ambient + diffuse + specular;
+
+    gl_Position = u_projection * u_view * worldPos;
 } 
